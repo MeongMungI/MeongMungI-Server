8e0446064c820912ff384177aae67d2e61c6a825
Junmo <hongjm1022@gmail.com>
2025-04-07 14:36:41 +0900

    Initial setting

---
+++ src/main/java/com/meongmungi/MeongmungiApplication.java
@@ -0,0 +1,13 @@
+package com.meongmungi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeongmungiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongmungiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meongmungi/MeongmungiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meongmungi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongmungiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
